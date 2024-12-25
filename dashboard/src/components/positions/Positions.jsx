878487dc8abaa17388e5a9246bfc570cfcdc8720
Rethanya <168968263+rethanyaaa@users.noreply.github.com>
2024-12-25 20:11:33 +0530

    Update Positions.jsx

--- dashboard/src/components/positions/Positions.jsx
+++ dashboard/src/components/positions/Positions.jsx
@@ -7,7 +7,7 @@ const Positions = () => {
   const [allPositions, setAllPositions] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/allPositions").then((res) => {
+    axios.get("https://bullnest-backend.onrender.com/allPositions").then((res) => {
       console.log(res.data);
       setAllPositions(res.data);
     });
